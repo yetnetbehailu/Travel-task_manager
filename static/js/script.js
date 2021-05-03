7796afb241a8b7cd7396100da1ddc7bb5224e42a
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-05-03 23:35:40 +0000

    Add categories droppdown

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $('.sidenav').sidenav({edge:'right'});
 $('.collapsible').collapsible();
 $('.tooltipped').tooltip();
+$("select").formSelect();
 $(".datepicker").datepicker({
         format: "dd mmm, yyyy",
         yearRange: 3,
