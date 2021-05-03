5af697cb210cbbdcaa1c807798997195c32c9bb8
yetnetbehailu <yetnet.behailu@hotmail.com>
2021-05-03 23:11:54 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
 $('.sidenav').sidenav({edge:'right'});
 $('.collapsible').collapsible();
 $('.tooltipped').tooltip();
+$(".datepicker").datepicker({
+        format: "dd mmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
